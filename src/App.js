7433ced4bc872b2215a57ef75c7c799436a65dce
kongou <giachan9@gmail.com>
2022-10-15 17:03:10 +0700

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './components/header/index'
+import Home from './pages/Home'
+import Footer from './components/footer/index'
+import Service from './pages/Service';
+import About from './pages/About';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/service' element={<Service />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
---
+++ src/pages/Service.jsx
@@ -0,0 +1,14 @@
+import { Box, Typography } from '@mui/material'
+import React from 'react'
+
+function Service() {
+  return (
+    <Box>
+        <Typography variant='h1'>
+            
+        </Typography>
+    </Box>
+  )
+}
+
+export default Service
